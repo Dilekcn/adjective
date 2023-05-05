826e8618865259f849f98c9540bca679e68e7826
Dilekcn <canturkdk@gmail.com>
2023-05-05 10:41:28 +0100

    .env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3010;
+const port = process.env.PORT || 3010;
 const pool = require("./db");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
